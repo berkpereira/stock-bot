17f15dd21e29f1135f8d38286ed4391b87a45545
Gabriel Pereira <berkpereira@gmail.com>
2022-09-19 12:13:11 +0100

    Renamed tweet timestamp module

--- timestamps.py
+++ timestamps.py
@@ -2,8 +2,9 @@
 # tweet ID number
 # Works for post-snowflake tweets (post 4 Nov 2010)
 # Read more: https://ws-dl.blogspot.com/2019/08/2019-08-03-tweetedat-finding-tweet.html#:~:text=According%20to%20Twitter's%20post%20on,Twitter%20epoch%20time%20of%201288834974657.
-def get_tweet_timestamp(tid):
+# Source: https://github.com/oduwsdl/tweetedat
+from datetime import fromtimestamp
+def get_utc_timestamp(tid):
     offset = 1288834974657
-    tstamp = (tid >> 22) + offset
-    utcdttime = datetime.utcfromtimestamp(tstamp/1000)
-    print(str(tid) + " : " + str(tstamp) + " => " + str(utcdttime))
+    unix_tstamp = (tid >> 22) + offset
+    return fromtimestamp(unix_tstamp)
