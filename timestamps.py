5a4d02674fc33ac4ecd411089265a7eee6a9e2b0
Gabriel Pereira <berkpereira@gmail.com>
2022-09-19 12:28:05 +0100

    Fixed tweet datetime module to output UTC datetime

--- timestamps.py
+++ timestamps.py
@@ -1,10 +1,10 @@
-# This function extracts the timestamp of a tweet using the
+# This function extracts the UTC datetime of a tweet using the
 # tweet ID number
 # Works for post-snowflake tweets (post 4 Nov 2010)
 # Read more: https://ws-dl.blogspot.com/2019/08/2019-08-03-tweetedat-finding-tweet.html#:~:text=According%20to%20Twitter's%20post%20on,Twitter%20epoch%20time%20of%201288834974657.
-# Source: https://github.com/oduwsdl/tweetedat
-from datetime import fromtimestamp
-def get_utc_timestamp(tid):
+# Code source: https://github.com/oduwsdl/tweetedat
+from datetime import datetime
+def get_utc_datetime(tid):
     offset = 1288834974657
     unix_tstamp = (tid >> 22) + offset
-    return fromtimestamp(unix_tstamp)
+    return datetime.utcfromtimestamp(unix_tstamp/1000.0)
